4c5a0ff02ab6081f35b96003028ee03632cd0726
Marc Hein <info@marc-hein.de>
2018-06-23 21:32:48 +0200

    Repaired current state of page

--- server/Main.js
+++ server/Main.js
@@ -2,7 +2,6 @@ var liveServer = require("live-server");
 
 var params = {
 	port: 8181, // Set the server port. Defaults to 8080.
-	host: "192.168.2.10", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
 	root: "dist/", // Set root directory that's being served. Defaults to cwd.
 	open: true, // When false, it won't load your browser by default.
 	ignore: "", // comma-separated string for paths to ignore
